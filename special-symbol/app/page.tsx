deb3e14a04cd3733ca7bdb22ce102fa9e5209a2b
DarrenChen77 <darrenchen@17.media>
2026-03-20 18:16:37 +0800

    add google verification

--- special-symbol/app/page.tsx
+++ special-symbol/app/page.tsx
@@ -7,8 +7,8 @@ export const metadata = {
     "免費特殊符號工具，包含愛心符號、箭頭符號、星星符號、可愛顏文字，一鍵複製，適用 IG、Threads、LINE、Facebook。",
   verification: {
     google: ehU1T8h_XzRdwyu3jiF31o0fGsbffh8jN2srktJihBc/>
-  },
-}
+  }
+};
 export default function Page() {
   return (
     <div>
